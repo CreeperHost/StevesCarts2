01bd7c292374e4755499dc5808f342a2c687c0dd
Gigabit101 <thegigabit101@gmail.com>
2022-06-16 18:41:14 +0100

    Add ModelLawnMower

--- src/main/java/vswe/stevescarts/client/models/ModelLawnMower.java
+++ src/main/java/vswe/stevescarts/client/models/ModelLawnMower.java
@@ -1,118 +1,94 @@
 package vswe.stevescarts.client.models;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.resources.ResourceLocation;
 import vswe.stevescarts.helpers.ResourceHelper;
 import vswe.stevescarts.modules.ModuleBase;
+import vswe.stevescarts.modules.realtimers.ModuleFlowerRemover;
 
 import java.util.ArrayList;
 
 public class ModelLawnMower extends ModelCartbase
 {
-    private static ResourceLocation texture;
-    private ArrayList<ModelPart> bladepins;
-
-    @Override
-    public ResourceLocation getResource(final ModuleBase module)
-    {
-        return ModelLawnMower.texture;
-    }
-
-    @Override
-    protected int getTextureWidth()
-    {
-        return 64;
-    }
-
-    @Override
-    protected int getTextureHeight()
-    {
-        return 32;
-    }
+    private static final ArrayList<ModelPart> bladepins = new ArrayList<>();
 
     public ModelLawnMower()
     {
-        bladepins = new ArrayList<>();
-        createSide(false);
-        createSide(true);
+        super(createBodyLayer().bakeRoot(), ResourceHelper.getResource("/models/lawnmowerModel.png"));
     }
 
-    private void createSide(final boolean opposite)
+    public static LayerDefinition createBodyLayer()
     {
-        //TODO
-        //        final ModelRenderer anchor = new ModelRenderer(this, 0, 0);
-        //        AddRenderer(anchor);
-        //        if (opposite)
-        //        {
-        //            anchor.yRot = 3.1415927f;
-        //        }
-        //        final ModelRenderer base = new ModelRenderer(this, 0, 0);
-        //        anchor.addChild(base);
-        //        fixSize(base);
-        //        base.addBox(-11.5f, -3.0f, -1.0f, 23, 6, 2, 0.0f);
-        //        base.setPos(0.0f, -1.5f, -9.0f);
-        //        for (int i = 0; i < 2; ++i)
-        //        {
-        //            final ModelRenderer arm = new ModelRenderer(this, 0, 8);
-        //            base.addChild(arm);
-        //            fixSize(arm);
-        //            arm.addBox(-8.0f, -1.5f, -1.5f, 16, 3, 3, 0.0f);
-        //            arm.setPos(-8.25f + i * 16.5f, 0.0f, -8.0f);
-        //            arm.yRot = 1.5707964f;
-        //            final ModelRenderer arm2 = new ModelRenderer(this, 0, 14);
-        //            arm.addChild(arm2);
-        //            fixSize(arm2);
-        //            arm2.addBox(-1.5f, -1.5f, -1.5f, 3, 3, 3, 0.0f);
-        //            arm2.setPos(6.5f, 3.0f, 0.0f);
-        //            arm2.zRot = 1.5707964f;
-        //            final ModelRenderer bladepin = new ModelRenderer(this, 0, 20);
-        //            arm2.addChild(bladepin);
-        //            fixSize(bladepin);
-        //            bladepin.addBox(-1.0f, -0.5f, -0.5f, 2, 1, 1, 0.0f);
-        //            bladepin.setPos(2.5f, 0.0f, 0.0f);
-        //            final ModelRenderer bladeanchor = new ModelRenderer(this, 0, 0);
-        //            bladepin.addChild(bladeanchor);
-        //            bladeanchor.yRot = 1.5707964f;
-        //            for (int j = 0; j < 4; ++j)
-        //            {
-        //                final ModelRenderer blade = new ModelRenderer(this, 0, 22);
-        //                bladeanchor.addChild(blade);
-        //                fixSize(blade);
-        //                blade.addBox(-1.5f, -1.5f, -0.5f, 8, 3, 1, 0.0f);
-        //                blade.setPos(0.0f, 0.0f, j * 0.01f);
-        //                blade.zRot = 1.5707964f * (j + i * 0.5f);
-        //                final ModelRenderer bladetip = new ModelRenderer(this, 0, 26);
-        //                blade.addChild(bladetip);
-        //                fixSize(bladetip);
-        //                bladetip.addBox(6.5f, -1.0f, -0.5f, 6, 2, 1, 0.0f);
-        //                bladetip.setPos(0.0f, 0.0f, 0.005f);
-        //            }
-        //            bladepins.add(bladepin);
-        //        }
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+        createSide(partdefinition, false);
+        createSide(partdefinition, true);
+
+        return LayerDefinition.create(meshdefinition, 64, 32);
     }
 
-    //TODO
-    //    @Override
-    //    public void applyEffects(final ModuleBase module, PoseStack matrixStack, VertexConsumer rtb, final float yaw, final float pitch, final float roll, final float partialtime)
-    //    {
-    //        super.applyEffects(module, matrixStack, rtb, yaw, pitch, roll, partialtime);
-    //        final float angle = (module == null) ? 0.0f : (((ModuleFlowerRemover) module).getBladeAngle() + partialtime * ((ModuleFlowerRemover) module).getBladeSpindSpeed());
-    //        for (int i = 0; i < bladepins.size(); ++i)
-    //        {
-    //            final ModelPart bladepin = bladepins.get(i);
-    //            if (i % 2 == 0)
-    //            {
-    //                bladepin.xRot = angle;
-    //            }
-    //            else
-    //            {
-    //                bladepin.xRot = -angle;
-    //            }
-    //        }
-    //    }
-
-    static
+    private static void createSide(PartDefinition partDefinition, final boolean opposite)
     {
-        ModelLawnMower.texture = ResourceHelper.getResource("/models/lawnmowerModel.png");
+        PartDefinition anchor = partDefinition.addOrReplaceChild("anchor" + opposite, CubeListBuilder.create().texOffs(0, 0)
+                        .addBox(-11.5f, -3.0f, -1.0f, 23, 6, 2).mirror(opposite),
+                PartPose.offsetAndRotation(0.0f, -1.5f, opposite ? -9.0f : 9.0f, 0, opposite ? 0 : 3.1415927f, 0));
+
+        for (int i = 0; i < 2; i++)
+        {
+            PartDefinition arm = anchor.addOrReplaceChild("arm" + i + opposite, CubeListBuilder.create().texOffs(0, 8)
+                            .addBox(-8.0f, -1.5f, -1.5f, 16, 3, 3).mirror(opposite),
+                    PartPose.offsetAndRotation(-8.25f + i * 16.5f, 0.0f, -8.0f, 0, 1.5707964f, 0));
+
+            PartDefinition arm2 = arm.addOrReplaceChild("arm2" + i + opposite, CubeListBuilder.create().texOffs(0, 8)
+                            .addBox(-1.5f, -1.5f, -1.5f, 3, 3, 3).mirror(opposite),
+                    PartPose.offsetAndRotation(6.5f, 3.0f, 0.0f, 0, 0, 1.5707964f));
+
+            PartDefinition bladePin = arm2.addOrReplaceChild("bladepin" + i + opposite, CubeListBuilder.create().texOffs(0, 8)
+                            .addBox(-1.0f, -0.5f, -0.5f, 2, 1, 1).mirror(opposite),
+                    PartPose.offsetAndRotation(2.5f, 0.0f, 0.0f, 0, 1.5707964f, 0));
+
+            PartDefinition bladeAnchor = bladePin.addOrReplaceChild("bladeanchor" + i + opposite,
+                    CubeListBuilder.create().texOffs(0, 0), PartPose.rotation(0, 0, 0));
+
+            for (int j = 0; j < 4; j++)
+            {
+                PartDefinition blade = bladeAnchor.addOrReplaceChild("blade" + j + opposite, CubeListBuilder.create().texOffs(0, 22)
+                                .addBox(-1.5f, -1.5f, -0.5f, 8, 3, 1).mirror(opposite),
+                        PartPose.offsetAndRotation(0.0f, 0.0f, 0.005f, 0, 0, 1.5707964f * (j + i * 0.5f)));
+
+                PartDefinition bladetip = blade.addOrReplaceChild("bladetip" + j + opposite, CubeListBuilder.create().texOffs(0, 22)
+                                .addBox(6.5f, -1.0f, -0.5f, 6, 2, 1).mirror(opposite),
+                        PartPose.offsetAndRotation(0.0f, 0.0f, 0.005f, 0, 0, 0));
+            }
+            bladepins.add(bladePin.bake(1, 1));
+        }
     }
+
+    @Override
+    public void applyEffects(final ModuleBase module, PoseStack matrixStack, VertexConsumer rtb, final float yaw, final float pitch, final float roll)
+    {
+        super.applyEffects(module, matrixStack, rtb, yaw, pitch, roll);
+        int partialtime = 0;
+        final float angle = (module == null) ? 0.0f : (((ModuleFlowerRemover) module).getBladeAngle() + partialtime * ((ModuleFlowerRemover) module).getBladeSpindSpeed());
+        for (int i = 0; i < bladepins.size(); ++i)
+        {
+            final ModelPart bladepin = bladepins.get(i);
+            if (i % 2 == 0)
+            {
+                bladepin.xRot = angle;
+            }
+            else
+            {
+                bladepin.xRot = -angle;
+            }
+        }
+    }
+
 }
