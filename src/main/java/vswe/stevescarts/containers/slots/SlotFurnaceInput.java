11c7c8737bfd9cf4644a5fc34ef22c2f2f8be068
Gigabit101 <thegigabit101@gmail.com>
2024-06-18 12:44:30 +0100

    Fix server side crash with smelter

--- src/main/java/vswe/stevescarts/containers/slots/SlotFurnaceInput.java
+++ src/main/java/vswe/stevescarts/containers/slots/SlotFurnaceInput.java
@@ -1,24 +1,22 @@
 package vswe.stevescarts.containers.slots;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.world.Container;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.ticks.ContainerSingleItem;
 import vswe.stevescarts.helpers.RecipeHelper;
 
 import javax.annotation.Nonnull;
 
 public class SlotFurnaceInput extends SlotFake {
-    public SlotFurnaceInput(final Container iinventory, final int i, final int j, final int k) {
+    private final Level level;
+
+    public SlotFurnaceInput(final Container iinventory, Level level, final int i, final int j, final int k) {
         super(iinventory, i, j, k);
+        this.level = level;
     }
 
     @Override
     public boolean mayPlace(@Nonnull ItemStack itemstack) {
-        Level level = Minecraft.getInstance().level;
         return RecipeHelper.findSmeltRecipe(itemstack, level).isPresent();
     }
 
