f212a59f8551c236c9c37af3e193555df718568a
brandon3055 <brandon4261@gmail.com>
2025-01-30 12:22:05 +1100

    Fix broken assembler slot exposure before gui is opened.

--- src/main/java/vswe/stevescarts/blocks/tileentities/TileEntityCartAssembler.java
+++ src/main/java/vswe/stevescarts/blocks/tileentities/TileEntityCartAssembler.java
@@ -1032,13 +1032,13 @@ public class TileEntityCartAssembler extends TileEntityBase implements WorldlyCo
     @Override
     public int @NotNull [] getSlotsForFace(@NotNull Direction direction)
     {
-        return new int[]{fuelSlot.index};
+        return new int[]{fuelSlot.getSlotIndex()};
     }
 
     @Override
     public boolean canPlaceItemThroughFace(int id, @NotNull ItemStack itemStack, @Nullable Direction direction)
     {
-        if(id == fuelSlot.index && FuelHelper.isItemFuel(itemStack, level)) return true;
+        if(id == fuelSlot.getSlotIndex() && FuelHelper.isItemFuel(itemStack, level)) return true;
         return false;
     }
 
