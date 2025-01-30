8987d6b46c93f7dceeda7275f81e9726e83b89a9
brandon3055 <brandon4261@gmail.com>
2025-01-30 11:56:59 +1100

    Prevent GUI crash if the wrong item somehow ends up in the wrong slot

--- src/main/java/vswe/stevescarts/client/guis/GuiCartAssembler.java
+++ src/main/java/vswe/stevescarts/client/guis/GuiCartAssembler.java
@@ -14,6 +14,7 @@ import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.ItemStack;
 import org.joml.Quaternionf;
 import org.joml.Vector3f;
 import vswe.stevescarts.StevesCarts;
@@ -83,9 +84,14 @@ public class GuiCartAssembler extends AbstractContainerScreen<ContainerCartAssem
         }
         else
         {
-            IModuleItem cartModule = (IModuleItem) this.assembler.getItem(0).getItem();
-            final ModuleData hulldata = cartModule.getModuleData();
-            if (hulldata == null || !(hulldata instanceof final ModuleDataHull hull))
+            ItemStack stack = this.assembler.getItem(0);
+            if (!(stack.getItem() instanceof IModuleItem cartModule)) {
+                this.hasErrors = false;
+                this.statusLog = lines;
+                return;
+            }
+            ModuleData hulldata = cartModule.getModuleData();
+            if (hulldata == null || !(hulldata instanceof ModuleDataHull hull))
             {
                 this.addText(lines, Localization.GUI.ASSEMBLER.INVALID_HULL.translate(), 10357518);
                 this.hasErrors = true;
