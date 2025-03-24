162b6deb94461c0c8416df5802484966c308bc9a
brandon3055 <brandon4261@gmail.com>
2025-03-24 17:33:42 +1100

    Fixed Adv Detector not releasing cart on redstone signal.

--- src/main/java/vswe/stevescarts/entities/EntityMinecartModular.java
+++ src/main/java/vswe/stevescarts/entities/EntityMinecartModular.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtUtils;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
@@ -1145,6 +1146,9 @@ public class EntityMinecartModular extends AbstractMinecart implements Container
                 module.writeToNBT(tagCompound, i);
             }
         }
+        if (disabledPos != null) {
+            tagCompound.put("disabled_pos", NbtUtils.writeBlockPos(disabledPos));
+        }
         return true;
     }
 
@@ -1182,6 +1186,9 @@ public class EntityMinecartModular extends AbstractMinecart implements Container
                 final ModuleBase module = modules.get(i);
                 module.readFromNBT(tagCompound, i);
             }
+        }
+        if (tagCompound.contains("disabled_pos")) {
+            disabledPos = NbtUtils.readBlockPos(tagCompound.getCompound("disabled_pos"));
         }
     }
 
