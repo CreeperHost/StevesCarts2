1fbe8de048ace828263af7d0f5e4a4ffabab606a
Gigabit101 <thegigabit101@gmail.com>
2026-09-23 11:46:25 +0100

    Fix cleaning module

--- src/main/java/vswe/stevescarts/modules/realtimers/ModuleCleaner.java
+++ src/main/java/vswe/stevescarts/modules/realtimers/ModuleCleaner.java
@@ -5,6 +5,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.Vec3;
 import vswe.stevescarts.api.modules.ModuleBase;
 import vswe.stevescarts.entities.ModularMinecart;
 
@@ -12,6 +13,8 @@ import javax.annotation.Nonnull;
 import java.util.List;
 
 public class ModuleCleaner extends ModuleBase {
+    private static final double MAX_SUCTION_SPEED = 0.35D;
+
     public ModuleCleaner(ModularMinecart cart) {
         super(cart);
     }
@@ -28,32 +31,22 @@ public class ModuleCleaner extends ModuleBase {
         clean();
     }
 
-    private double calculatemotion(final double dif) {
-        if (dif > -0.5D && dif < 0.5D) {
-            return 0;
-        }
-
-        return 1 / (dif * 2);
-    }
-
     private void suck() {
         final List<ItemEntity> list = getCart().level().getEntitiesOfClass(ItemEntity.class, getCart().getBoundingBox().inflate(3.0, 1.0, 3.0));
         for (ItemEntity eItem : list) {
-            if (!eItem.isPickable()) {
-                double difX = getCart().blockPosition().getX() - eItem.blockPosition().getX();
-                double difY = getCart().blockPosition().getY() - eItem.blockPosition().getY();
-                double difZ = getCart().blockPosition().getZ() - eItem.blockPosition().getZ();
-                //TODO, Why is this commented out?
-                //				eItem.moveTo(calculatemotion(difX), calculatemotion(difY), calculatemotion(difZ));
-                //				eItem.xOld += calculatemotion(difX);
-                //				eItem.yo += calculatemotion(difY);
-                //				eItem.zo += calculatemotion(difZ);
+            Vec3 direction = getCart().position().add(0.0D, 0.35D, 0.0D).subtract(eItem.position());
+            if (direction.lengthSqr() < 0.01D) continue;
+
+            Vec3 motion = eItem.getDeltaMovement().scale(0.8D).add(direction.normalize().scale(0.12D));
+            if (motion.lengthSqr() > MAX_SUCTION_SPEED * MAX_SUCTION_SPEED) {
+                motion = motion.normalize().scale(MAX_SUCTION_SPEED);
             }
+            eItem.setDeltaMovement(motion);
         }
     }
 
     private void clean() {
-        final List<ItemEntity> list = getCart().level().getEntitiesOfClass(ItemEntity.class, getCart().getBoundingBox().inflate(3.0, 1.0, 3.0));
+        final List<ItemEntity> list = getCart().level().getEntitiesOfClass(ItemEntity.class, getCart().getBoundingBox().inflate(0.6D, 0.75D, 0.6D));
         for (ItemEntity eItem : list) {
             if (!eItem.isPickable() && !eItem.isRemoved()) {
                 int stackSize = eItem.getItem().getCount();
