80cf5dde8a279b1deeadd473354c552103f836da
Gigabit101 <thegigabit101@gmail.com>
2026-09-23 11:25:56 +0100

    Fix drill not using fortune

--- src/main/java/vswe/stevescarts/modules/workers/tools/ModuleDrill.java
+++ src/main/java/vswe/stevescarts/modules/workers/tools/ModuleDrill.java
@@ -5,6 +5,8 @@ import net.creeperhost.polylib.data.serializable.BooleanData;
 import net.creeperhost.polylib.helpers.LevelHelper;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.Container;
@@ -14,6 +16,8 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.BaseRailBlock;
@@ -66,6 +70,18 @@ public abstract class ModuleDrill extends ModuleTool implements IActivatorModule
     }
 
     @Override
+    public float getMaxSpeed() {
+        if (isDrillEnabled() && !getCart().level().isClientSide()) {
+            BlockPos next = getNextblock();
+            if (!BaseRailBlock.isRail(getCart().level(), next)
+                    && !BaseRailBlock.isRail(getCart().level(), next.below())) {
+                return 0.0f;
+            }
+        }
+        return super.getMaxSpeed();
+    }
+
+    @Override
     public void init() {
         super.init();
         for (final ModuleBase module : getCart().modules()) {
@@ -87,6 +103,11 @@ public abstract class ModuleDrill extends ModuleTool implements IActivatorModule
     @Override
     public boolean work() {
         Level world = getCart().level();
+        if (!getCart().isOnRails()) {
+            stopDrill();
+            stopWorking();
+            return false;
+        }
         if (!isDrillEnabled()) {
             stopDrill();
             stopWorking();
@@ -134,11 +155,13 @@ public abstract class ModuleDrill extends ModuleTool implements IActivatorModule
         if (BaseRailBlock.isRail(getCart().level(), next) || BaseRailBlock.isRail(getCart().level(), next.below())) {
             return new int[]{0, blocksOnTop() - 1, 1};
         }
-        if (next.getY() > yTarget) {
-            return new int[]{-1, blocksOnTop() - 1, 1};
-        }
-        if (next.getY() < yTarget) {
-            return new int[]{1, blocksOnTop() + 1, 0};
+        if (intelligence != null) {
+            if (next.getY() > yTarget) {
+                return new int[]{-1, blocksOnTop() - 1, 1};
+            }
+            if (next.getY() < yTarget) {
+                return new int[]{1, blocksOnTop() + 1, 0};
+            }
         }
         return new int[]{0, blocksOnTop() - 1, 1};
     }
@@ -199,8 +222,8 @@ public abstract class ModuleDrill extends ModuleTool implements IActivatorModule
         int fortune = (enchanter != null) ? enchanter.getFortuneLevel() : 0;
         ItemStack tool = new ItemStack(Items.DIAMOND_PICKAXE);
         if (fortune > 0) {
-            //TODO
-//            tool.enchant(Enchantments.BLOCK_FORTUNE, fortune);
+            Registry<Enchantment> enchantments = getCart().registryAccess().lookupOrThrow(Registries.ENCHANTMENT);
+            tool.enchant(enchantments.getOrThrow(Enchantments.FORTUNE), fortune);
         }
 
         LootParams.Builder builder = new LootParams.Builder((ServerLevel) world)
